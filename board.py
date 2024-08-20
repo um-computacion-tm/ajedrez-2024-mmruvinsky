056dab810bc2997241f71b25919681ef322eeda7
Miguel <mmruvinsky@gmail.com>
2024-08-20 09:27:39 -0300

    Creación de archivos

---
+++ board.py
@@ -0,0 +1,17 @@
+pieces import Rook
+
+class Board:
+    def __init__(self):
+        self.__positions__ = []
+        for _ in range(8):
+            col = []
+            for _ in range(8):
+                col.append(None)
+            self.positions.append(col)
+        self.__positions__[0][0] = Rook("BLACK") #Black
+        self.__positions__[0][7] = Rook("BLACK") #Black
+        self.__positions__[7][7] = Rook("WHITE") #White
+        self.__positions__[7][0] = Rook("WHITE") #White
+
+    def get_piece(self, row, col):
+        return self.board.__positions__[row][col]
