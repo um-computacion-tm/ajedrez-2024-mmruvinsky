d7a4e94e0ae77ccdd83c1f813792d24b896758f1
Miguel <mmruvinsky@gmail.com>
2024-08-26 22:15:29 -0300

    Agregando las piezas que faltan y completando archivo board

---
+++ bishop.py
@@ -0,0 +1,5 @@
+from piece import Piece
+
+
+class Bishop(Piece):
+    ...
--- main.py
+++
@@ -1,8 +0,0 @@
-def suma(a, b):
-    return a + b
-
-def main():
-    print(suma(1, 2))
-
-if __name__ == "__main__":
-    main()
